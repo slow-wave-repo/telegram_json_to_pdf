a9fd0e6719f662a403977da50719e159cbdd4f73
xxnvt4 <teodantes@gmail.com>
2024-07-18 20:09:57 +0200

    class PersonalChat

--- json-pdf.py
+++ json-pdf.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
 
 
+import glob
 import json
 import locale
 import os
-import glob
-from pathlib import Path
+import shutil
+import sys
 from datetime import datetime
+from pathlib import Path
 
-from reportlab.lib.enums import TA_CENTER, TA_RIGHT, TA_LEFT
+import emoji
+from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.units import cm
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, HRFlowable
 
 
-class Settings:
+class JsonPdf:
     TEXT = 'text'
     TITLE = 'title_style'
     PERIOD = 'period_style'
@@ -25,23 +29,49 @@ class Settings:
     SELF_MESSAGE = 'self_message_style'
     PARTNER_MESSAGE = 'partner_message_style'
 
-    font_regular_path = "regular.ttf"
-    font_bold_path = "bold.ttf"
+    REGULAR = "regular.ttf"
+    BOLD = "bold.ttf"
 
-    pdfmetrics.registerFont(TTFont('Font', font_regular_path))
-    pdfmetrics.registerFont(TTFont('FontBold', font_bold_path))
+    locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
 
-    text_style = ParagraphStyle(name='TextStyle', fontName='Font', fontSize=14)
-    title_style = ParagraphStyle(name='TitleStyle', fontName='FontBold', fontSize=40, alignment=TA_CENTER, bold=True,
+    pdfmetrics.registerFont(TTFont('Font', REGULAR))
+    pdfmetrics.registerFont(TTFont('FontBold', BOLD))
+
+    text_style = ParagraphStyle(name='TextStyle',
+                                fontName='Font',
+                                fontSize=12)
+    title_style = ParagraphStyle(name='TitleStyle',
+                                 fontName='FontBold',
+                                 fontSize=40,
+                                 alignment=TA_CENTER,
+                                 bold=True,
                                  textTransform='uppercase')
-    period_style = ParagraphStyle(name='PeriodStyle', fontName='Font', fontSize=16, alignment=TA_CENTER)
-    date_style = ParagraphStyle(name='DateStyle', fontName='Font', fontSize=14, alignment=TA_RIGHT)
-    name_style = ParagraphStyle(name='NameStyle', fontName='Font', fontSize=14, alignment=TA_CENTER,
+    period_style = ParagraphStyle(name='PeriodStyle',
+                                  fontName='Font',
+                                  fontSize=10,
+                                  alignment=TA_CENTER)
+    date_style = ParagraphStyle(name='DateStyle',
+                                fontName='Font',
+                                fontSize=8,
+                                alignment=TA_CENTER,
+                                textColor='grey')
+    name_style = ParagraphStyle(name='NameStyle',
+                                fontName='Font',
+                                fontSize=12,
+                                alignment=TA_CENTER,
                                 textTransform='uppercase')
-    self_message_style = ParagraphStyle(name='SelfMessageStyle', fontName='FontBold', bold=True, fontSize=14, alignment=TA_RIGHT)
-    partner_message_style = ParagraphStyle(name='PartnerMessageStyle', fontName='Font', fontSize=14)
+    self_message_style = ParagraphStyle(name='SelfMessageStyle',
+                                        fontName='Font',
+                                        bold=True,
+                                        fontSize=11,
+                                        alignment=TA_RIGHT,
+                                        leading=15)
+    partner_message_style = ParagraphStyle(name='PartnerMessageStyle',
+                                           fontName='Font',
+                                           fontSize=11,
+                                           textColor='grey')
 
-    def style(self, message: str, type) -> Paragraph:
+    def style(self, message: str, type: str) -> Paragraph:
         if type == 'text':
             return Paragraph(message, self.text_style)
         elif type == 'title':
@@ -59,45 +89,23 @@ class Settings:
         else:
             return self.style(message, 'text')
 
-
-
-class PersonalChat(Settings):
-    def __init__(self, file):
-        with open(file, 'r', encoding='utf-8') as opened_file:
-            self.data = json.load(opened_file)
-
-        self.name = self.data['name']
-        self.earliest_date, self.latest_date = self.get_time_borders(self.data)
-        self.time_period_inside = f'{self.format_date_inside(self.earliest_date)} — {self.format_date_inside(self.latest_date)}'
-        self.time_period_name = f'{self.format_date_name(self.earliest_date)} — {self.format_date_name(self.latest_date)}'
-
-    def choose_role_tag(self, message, name, story):
-        if name == self.data['name']:
-            story.append(self.style(message, 'partner_message'))
-        else:
-            story.append(self.style(message, 'self_message'))
-
-    def format_date_inside(self, date_str):
+    def format_date_inside(self, date_str: str) -> str:
         date_obj = datetime.fromisoformat(date_str)
-        formatted_date = date_obj.strftime('%d %B %Y')
+        formatted_date = date_obj.strftime('%e %B %Y')
 
         return formatted_date
 
-    def format_date_name(self, date_str):
+    def format_date_name(self, date_str: str) -> str:
         date_obj = datetime.fromisoformat(date_str)
         formatted_date = date_obj.strftime("%d.%m.%Y")
 
         return formatted_date
 
-    def check_if_json(self, file_path):
-        file = Path(file_path)
-        return file.suffix == '.json'
-
-    def get_time_borders(self, data):
+    def get_time_borders(self, input_data):
         earliest_date = None
         latest_date = None
 
-        for message in data['messages']:
+        for message in input_data['messages']:
             current_date = message['date']
 
             if earliest_date is None or current_date < earliest_date:
@@ -108,34 +116,68 @@ class PersonalChat(Settings):
 
         return earliest_date, latest_date
 
+    def check_message(self, message: str) -> str:
+        result = message.replace('\n', '<br/><br/>')
+
+        return emoji.replace_emoji(result, '(EMOJI)')
+
+    def check_if_json(self, file_path: str) -> bool:
+        file = Path(file_path)
+        return file.suffix == '.json'
+
+
+class PersonalChatJsonPdf(JsonPdf):
+    def __init__(self, input_file: str):
+        with open(input_file, 'r', encoding='utf-8') as opened_file:
+            self.data = json.load(opened_file)
+
+        self.name = self.data['name']
+        self.earliest_date, self.latest_date = self.get_time_borders(self.data)
+        self.time_period_inside = f'{self.format_date_inside(self.earliest_date)} — {self.format_date_inside(self.latest_date)}'
+        self.time_period_name = f'{self.format_date_name(self.earliest_date)} — {self.format_date_name(self.latest_date)}'
+
+    def choose_role_tag(self, message, name: str, story: list):
+        if name == self.data['name']:
+            story.append(self.style(message, 'partner_message'))
+        else:
+            story.append(self.style(message, 'self_message'))
 
     def make_pdf(self):
         previous_date = None
 
+        pdf_output_path = "result.pdf"
+        doc = SimpleDocTemplate(pdf_output_path, pagesize=A4)
         story = []
 
         story.append(self.style(f"{self.data['name']}", 'title'))
         story.append(Spacer(1, 40))
         story.append(self.style(f"{self.time_period_inside}", 'period'))
-        story.append(Spacer(1, 16))
 
         media_types = {
-            'voice_message': '— VOICE MESSAGE —',
-            'video_message': '— VIDEO MESSAGE —',
-            'video': '— VIDEO —',
-            'photo': '— PHOTO —',
-            'sticker': '— STICKER —',
-            'other': '— OTHER —',
-            'call': '— CALL —'
+            'voice_message': '(VOICE MESSAGE)',
+            'video_message': '(VIDEO MESSAGE)',
+            'video': '(VIDEO)',
+            'photo': '(PHOTO)',
+            'sticker': '(STICKER)',
+            'other': '(OTHER)',
+            'call': '(CALL)'
         }
 
         for message in self.data['messages']:
             if self.format_date_inside(message['date']) != previous_date:
                 story.append(Spacer(1, 48))
 
-                story.append(self.style(f"{self.format_date_inside(message['date'])}", 'date_style'))
+                story.append(
+                    HRFlowable(width="50%", thickness=1, lineCap='round', spaceBefore=0.3 * cm,
+                               spaceAfter=0.5 * cm, hAlign='CENTER'))
 
-                story.append(Spacer(1, 36))
+                story.append(self.style(f"— {self.format_date_inside(message['date'])} —", 'date_style'))
+
+                story.append(
+                    HRFlowable(width="50%", thickness=1, lineCap='round', spaceBefore=0.3 * cm,
+                               spaceAfter=0.5 * cm, hAlign='CENTER'))
+
+                story.append(Spacer(1, 30))
 
                 previous_date = self.format_date_inside(message['date'])
 
@@ -144,9 +186,8 @@ class PersonalChat(Settings):
             elif isinstance(message['text'], list):
                 self.choose_role_tag(media_types['call'], message['from'], story)
             else:
-                self.choose_role_tag(message['text'], message['from'], story)
-                story.append(Spacer(1, 12))
-                story.append(Spacer(1, 12))
+                self.choose_role_tag(self.check_message(message['text']), message['from'], story)
+                story.append(Spacer(1, 20))
             if 'media_type' in message:
                 try:
                     self.choose_role_tag(media_types[message['media_type']], message['from'], story)
@@ -155,34 +196,69 @@ class PersonalChat(Settings):
 
         doc.build(story)
 
-        new_name = f"{self.name}, {self.time_period_name}"
+        new_name = f"{self.name}, {self.time_period_name}.pdf"
 
         os.rename(pdf_output_path, new_name)
 
 
-locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
+class ChooseJson:
+    def __init__(self, path='.'):
+        self.directory_path = path
 
-directory_path = '/home/xxvnt4/Desktop'
-pdf_output_path = "result.pdf"
-doc = SimpleDocTemplate(pdf_output_path, pagesize=A4)
+        if os.path.isdir(self.directory_path):
+            self.menu = {}
 
-json_files = glob.glob(os.path.join(directory_path, '**/*.json'), recursive=True)
+            self.json_files = glob.glob(os.path.join(self.directory_path, '**/*.json'), recursive=True)
+            counter = 0
 
-print('List of Telegram-JSONs:\n')
-counter = 0
+            for json_file in self.json_files:
+                counter += 1
 
-for json_file in json_files:
-    counter += 1
+                self.menu[counter] = json_file
 
-    with open(json_file, 'r', encoding='utf-8') as file:
-        data = json.load(file)
+            self.fill_terminal_width()
+            print('Telegram-JSON to PDF Converter')
+            self.fill_terminal_width()
+            print()
 
-        if 'type' in data.keys() and 'id' in data.keys() and 'messages' in data.keys():
-            print(f'({counter}) {json_file} \n')
+            for item in self.menu:
+                print(f'{item} -- {self.menu[item]} \n')
 
-process_file = PersonalChat('result.json')
-process_file.make_pdf()
+            self.fill_terminal_width()
+            print()
+            print('0 -- Exit')
+
+            process_file = self.choose()
+        else:
+            process_file = PersonalChatJsonPdf(self.directory_path)
+
+        process_file.make_pdf()
+
+    def fill_terminal_width(self):
+        print('-' * shutil.get_terminal_size()[0])
+
+    def choose(self):
+        print()
+        self.fill_terminal_width()
+
+        try:
+            option = int(input('\nChoose: '))
+        except ValueError:
+            return self.choose()
+
+        if option == 0:
+            quit()
+        elif option not in list(self.menu):
+            return self.choose()
+        else:
+            return PersonalChatJsonPdf(self.menu[int(option)])
 
 
 
-print(f"{data['name']} is ready!")
+if __name__ == '__main__':
+    argument = sys.argv
+
+    if len(argument) > 1:
+        ChooseJson(argument[1])
+    else:
+        ChooseJson()
