c96b1a07c03a9197ea57d084748320df6c290fc6
xxnvt4 <teodantes@gmail.com>
2024-07-19 23:31:23 +0200

    Add class PrivateGroupJsonPdf

--- json-pdf.py
+++ json-pdf.py
@@ -11,7 +11,10 @@ from datetime import datetime
 from pathlib import Path
 import argparse
 
+import matplotlib.font_manager as fm
+
 import emoji
+from PIL import ImageFont
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import ParagraphStyle
@@ -30,8 +33,8 @@ class JsonPdf:
     SELF_MESSAGE = 'self_message_style'
     PARTNER_MESSAGE = 'partner_message_style'
 
-    REGULAR = "regular.ttf"
-    BOLD = "bold.ttf"
+    REGULAR = 'fonts/regular.ttf'
+    BOLD = 'fonts/bold.ttf'
 
     locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
 
@@ -70,7 +73,21 @@ class JsonPdf:
     partner_message_style = ParagraphStyle(name='PartnerMessageStyle',
                                            fontName='Font',
                                            fontSize=11,
-                                           textColor='grey')
+                                           textColor='grey',
+                                           leading=15)
+    actor_style = ParagraphStyle(name='ActorStyle',
+                                 fontName='Font',
+                                 fontSize=13,
+                                 textTransform='uppercase',
+                                 alignment=TA_CENTER,
+                                 textColor='grey',
+                                 spaceBefore=30,
+                                 spaceAfter=20)
+
+    actor_message_style = ParagraphStyle(name='ActorMessageStyle',
+                                         fontName='Font',
+                                         fontSize=12,
+                                         leading=15)
 
     media_types = {
         'voice_message': '(VOICE MESSAGE)',
@@ -81,6 +98,18 @@ class JsonPdf:
         'other': '(OTHER)',
         'call': '(CALL)'
     }
+
+    def __init__(self, input_file, directory_path, path_save=f'/home/{os.getlogin()}/Desktop'):
+
+        self.directory_path = directory_path
+        self.input_file = input_file
+        self.path_save = path_save
+
+        self.name = self.input_file['name']
+        self.earliest_date, self.latest_date = self.get_time_borders(self.input_file)
+        self.time_period_inside = f'{self.format_date_inside(self.earliest_date)} — {self.format_date_inside(self.latest_date)}'
+        self.time_period_name = f'{self.format_date_name(self.earliest_date)} — {self.format_date_name(self.latest_date)}'
+
 
     def style(self, message: str, type: str) -> Paragraph:
         if type == 'text':
@@ -97,6 +126,10 @@ class JsonPdf:
             return Paragraph(message, self.partner_message_style)
         elif type == 'date_style':
             return Paragraph(message, self.date_style)
+        elif type == 'actor_style':
+            return Paragraph(message, self.actor_style)
+        elif type == 'actor_message_style':
+            return Paragraph(message, self.actor_message_style)
         else:
             return self.style(message, 'text')
 
@@ -141,19 +174,9 @@ class JsonPdf:
 
 
 class PersonalChatJsonPdf(JsonPdf):
-    def __init__(self, input_file: str, path_save=f'/home/{os.getlogin()}/Desktop'):
-        with open(input_file, 'r', encoding='utf-8') as opened_file:
-            self.data = json.load(opened_file)
-
-        self.input_file = input_file
-        self.name = self.data['name']
-        self.earliest_date, self.latest_date = self.get_time_borders(self.data)
-        self.time_period_inside = f'{self.format_date_inside(self.earliest_date)} — {self.format_date_inside(self.latest_date)}'
-        self.time_period_name = f'{self.format_date_name(self.earliest_date)} — {self.format_date_name(self.latest_date)}'
-        self.path_save = path_save
 
     def choose_role_tag(self, message, name: str, story: list):
-        if name == self.data['name']:
+        if name == self.input_file['name']:
             story.append(self.style(message, 'partner_message'))
         else:
             story.append(self.style(message, 'self_message'))
@@ -169,11 +192,11 @@ class PersonalChatJsonPdf(JsonPdf):
             doc = SimpleDocTemplate(pdf_output_path, pagesize=A4)
             story = []
 
-            story.append(self.style(f"{self.data['name']}", 'title'))
+            story.append(self.style(f"{self.input_file['name']}", 'title'))
             story.append(Spacer(1, 40))
             story.append(self.style(f"{self.time_period_inside}", 'period'))
 
-            for message in self.data['messages']:
+            for message in self.input_file['messages']:
                 if self.format_date_inside(message['date']) != previous_date:
                     story.append(Spacer(1, 48))
 
@@ -209,18 +232,122 @@ class PersonalChatJsonPdf(JsonPdf):
             new_name = f'{self.path_save}/JSONtoPDF/{self.name}/{self.name}, {self.time_period_name}.pdf'
 
             os.rename(pdf_output_path, new_name)
-            shutil.move(self.input_file, f'{self.path_save}/JSONtoPDF/{self.name}/{self.name}, {self.time_period_name}.json')
+            shutil.copy(self.directory_path, f'{self.path_save}/JSONtoPDF/{self.name}/{self.name}, {self.time_period_name}.json')
             os.system(f'xdg-open "{new_name}"')
 
             os.system('clear')
             print(f"{self.name}, {self.time_period_name}.pdf is ready!")
 
             if self.path_save == f'/home/{os.getlogin()}/Desktop':
-                print(f'\nSaved to Desktop.')
+                print(f'\nCopied to Desktop.')
             elif self.path_save == f'.':
-                print(f'\nSave to {os.getcwd()}')
+                print(f'\nCopied to {os.getcwd()}')
             else:
-                print(f'\nSave to {self.path_save}')
+                print(f'\nCopied to {self.path_save}')
+        else:
+            os.system('clear')
+            print('\nThis PDF-file already exists!')
+
+        input('\nPress any key to close...')
+        os.system('clear')
+
+
+class PrivateGroupJsonPdf(JsonPdf):
+
+    def make_pdf(self):
+        if self.check_if_pdf_exists(f'{self.path_save}/JSONtoPDF/{self.name}/{self.name}, {self.time_period_name}.pdf'):
+            previous_date = None
+            previous_actor = None
+
+            path = Path(f'{self.path_save}/JSONtoPDF/{self.name}/')
+            path.mkdir(parents=True, exist_ok=True)
+
+            pdf_output_path = f'{self.path_save}/JSONtoPDF/{self.name}/{self.name}, {self.time_period_name}.pdf'
+            doc = SimpleDocTemplate(pdf_output_path, pagesize=A4)
+            story = []
+
+            story.append(story.append(self.style(f"{self.input_file['name']}", 'title')))
+            story.append(Spacer(1, 40))
+            story.append(self.style(f"{self.time_period_inside}", 'period'))
+
+            for message in self.input_file['messages']:
+                if self.format_date_inside(message['date']) != previous_date:
+                    story.append(Spacer(1, 48))
+
+                    story.append(
+                        HRFlowable(width="50%", thickness=1, lineCap='round', spaceBefore=0.3 * cm,
+                                   spaceAfter=0.5 * cm, hAlign='CENTER'))
+
+                    story.append(self.style(f"— {self.format_date_inside(message['date'])} —", 'date_style'))
+
+                    story.append(
+                        HRFlowable(width="50%", thickness=1, lineCap='round', spaceBefore=0.3 * cm,
+                                   spaceAfter=0.5 * cm, hAlign='CENTER'))
+
+                    story.append(Spacer(1, 30))
+
+                    previous_date = self.format_date_inside(message['date'])
+
+                if 'actor' in list(message.keys()):
+                    if message['actor'] != previous_actor:
+                        story.append(Spacer(1, 20))
+                        story.append(self.style(self.check_message(message["actor"]), 'actor_style'))
+                        story.append(Spacer(1, 20))
+
+                        previous_actor = message['actor']
+
+                if 'from' in list(message.keys()):
+                    if message['from'] != previous_actor:
+                        story.append(Spacer(1, 20))
+                        story.append(self.style(self.check_message(message["from"]), 'actor_style'))
+                        story.append(Spacer(1, 20))
+
+                        previous_actor = message['from']
+
+                if message['type'] == 'service' and (message['action'] == 'invite_members' or message['action'] == 'create_group'):
+                    invites_members = message['members']
+                    edited_invites_members = ','.join(invites_members)
+                    # print(self.check_message(edited_invites_members))
+                    story.append(self.style(f'(INVITES: {self.check_message(edited_invites_members)})', 'actor_message_style'))
+
+                elif message['type'] == 'service' and message['action'] == 'join_group_by_link':
+                    story.append(self.style('(JOINED VIA LINK)', 'actor_message_style'))
+
+                elif message['type'] == 'message' and 'photo' in list(message):
+                    story.append(self.style(self.check_message(f'(PHOTO) {message["text"]}'), 'actor_message_style'))
+
+                elif message['type'] == 'message' and 'video' in list(message):
+                    story.append(self.style(self.check_message(f'(VIDEO) {message["text"]}'), 'actor_message_style'))
+
+                elif message['type'] == 'message' and 'audio' in list(message):
+                    story.append(self.style(self.check_message(f'(AUDIO) {message["text"]}'), 'actor_message_style'))
+
+                elif message['type'] == 'message' and isinstance(message['text'], list):
+                    story.append(self.style(self.check_message(f'{message["text"][0]} {message["text"][1]}'), 'actor_message_style'))
+                else:
+                    story.append(self.style(self.check_message(message['text']), 'actor_message_style'))
+
+
+
+
+            doc.build(story)
+
+            new_name = f'{self.path_save}/JSONtoPDF/{self.name}/{self.name}, {self.time_period_name}.pdf'
+
+            os.rename(pdf_output_path, new_name)
+            shutil.copy(self.directory_path,
+                        f'{self.path_save}/JSONtoPDF/{self.name}/{self.name}, {self.time_period_name}.json')
+            os.system(f'xdg-open "{new_name}"')
+
+            # os.system('clear')
+            print(f"{self.name}, {self.time_period_name}.pdf is ready!")
+
+            if self.path_save == f'/home/{os.getlogin()}/Desktop':
+                print(f'\nCopied to Desktop.')
+            elif self.path_save == f'.':
+                print(f'\nCopied to {os.getcwd()}')
+            else:
+                print(f'\nCopied to {self.path_save}')
         else:
             os.system('clear')
             print('\nThis PDF-file already exists!')
@@ -255,15 +382,23 @@ class ChooseJson:
 
             self.fill_terminal_width()
             print('\n0 -- Exit')
-
-            process_file = self.choose()
+            self.choose()
         else:
-            process_file = PersonalChatJsonPdf(self.directory_path, self.path_save)
-
-        process_file.make_pdf()
+            with open(self.directory_path, 'r', encoding='utf-8') as opened_file:
+                self.data = json.load(opened_file)
+                self.determine_type_of_chat(self.directory_path)
 
     def fill_terminal_width(self):
         print('-' * shutil.get_terminal_size()[0])
+
+    def determine_type_of_chat(self, path):
+        if self.data['type'] == 'personal_chat':
+            PersonalChatJsonPdf(self.data, path, self.path_save).make_pdf()
+        elif self.data['type'] == 'private_group':
+            PrivateGroupJsonPdf(self.data, path, self.path_save).make_pdf()
+        else:
+            pass
+
 
     def choose(self):
         print()
@@ -278,16 +413,16 @@ class ChooseJson:
             os.system('clear')
             quit()
         elif option not in list(self.menu):
-            return self.choose()
+            self.choose()
         else:
-            return PersonalChatJsonPdf(self.menu[int(option)], self.path_save)
+            with open(self.menu[int(option)], 'r', encoding='utf-8') as opened_file:
+                self.data = json.load(opened_file)
+                self.determine_type_of_chat(self.menu[int(option)])
 
 
 if __name__ == '__main__':
     os.system('clear')
-
     parser = argparse.ArgumentParser()
-
     parser.add_argument('-f', '--file', type=str)
     parser.add_argument('-d', '--destination', type=str, )
     args = parser.parse_args()
